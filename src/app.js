c2f5153fd4f65b43b0a18a5cd82d3584df1ce693
priyanshukr449 <priyanshukr449@gmail.com>
2023-11-07 11:07:10 +0530

    utils code

---
+++ src/app.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// Talk from frontend to backend allow
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
+
+// To take data as a json form
+app.use(
+    express.json({
+        limit: "16kb",
+    })
+);
+
+// To take data as a Url form
+app.use(
+    express.urlencoded({
+        extended: true,
+        limit: "16kb",
+    })
+);
+
+// To allow some data for public (any one can access)
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,20 @@
-import connectDB from './db/index.js';
-import dotenv from "dotenv"
+import app from "./app.js";
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
 
 dotenv.config({
-    path: './env'
-})
+    path: "./env",
+});
 
-connectDB();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 4000, () => {
+            console.log(`Server is running at port : ${process.env.PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log("MongoDB connection Failed !!!", err);
+    });
 
 
 
